159b7d4f8c1bef1c6d9b7368ccb07bd22854fe70
Bantc <bantczak@hotmail.com>
2025-03-01 19:55:12 +0100

    Add bash script

---
+++ change_badge.sed
@@ -0,0 +1,12 @@
+/grouped-window-list-badge/ {
+    p;
+    n;
+    p;
+    n;
+    /242424/ {
+        s/242424/00AA00/;
+        p;
+        d;
+    }
+}
+p;
---
+++ green_cinnamon.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Update blue toogle-on button with green version.
+cp -f ./toggle-on.svg /usr/share/cinnamon/theme
+
+# Make Mint-Y-Dark similar green accents and get rid of light grey panel and dialog accents.
+sed -i 's/#3584e4/#00AA00/g' /usr/share/cinnamon/theme/cinnamon.css
+sed -i 's/#3584e4/#555555/g' /usr/share/cinnamon/theme/cinnamon.css
+sed -i 's/#454545/#222223/g' /usr/share/cinnamon/theme/cinnamon.css
+sed -i 's/#333333/#222223/g' /usr/share/cinnamon/theme/cinnamon.css
+sed -i 's/#78aeed/#00AA00/g' /usr/share/cinnamon/theme/cinnamon.css
+
+# Green window tile preview.
+sed -i 's/background-color: rgba(120, 174, 237, 0.2)/background-color: rgba(0, 170, 0, 0.2)/g' /usr/share/cinnamon/theme/cinnamon.css
+sed -i 's/border: 1px solid rgba(120, 174, 237, 0.7)/border: 1px solid rgba(0, 170, 0, 0.7)/g' /usr/share/cinnamon/theme/cinnamon.css
+
+# Mint-Y-Dark green running application instances counter badge.
+sed -i -n -f ./change_badge.sed /usr/share/cinnamon/theme/cinnamon.css
+
